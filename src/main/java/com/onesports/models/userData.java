f45c654551f9fd8dc91e743aa77ae89e7a8ca44b
sunil <sunilrenati10@gmail.com>
2026-06-09 17:40:55 +0530

    fixed stale elements

--- src/main/java/com/onesports/models/userData.java
+++ src/main/java/com/onesports/models/userData.java
@@ -16,29 +16,6 @@ public class UserData {
     private String spec2;
     private String password;
 
-     // Step1 - Personal Details
-    private String photoPath;
-    private String aadhaarFilePath;
-    private String dob;
-    private String gender;
-    private String aadhaarNumber;
-    private String bloodGroup;
-    private String admissionType;
-    private String transportRequired;
-    private String hostelRequired;
-
-    // Parent Details
-    private String fatherName;
-    private String fatherEmail;
-    private String fatherMobile;
-
-    private String motherName;
-    private String motherEmail;
-    private String motherMobile;
-
-    //     public UserData() {
-    // }   
-
     public UserData(String initials, String fullName, String surname, String email,
                     String mobile, String altCode, String altMobile,
                     String state, String city, String course,
@@ -79,96 +56,5 @@ public class UserData {
     public String getSpec1() { return spec1; }
     public String getSpec2() { return spec2; }
     public String getPassword() { return password; }
-
-    public String getPhotoPath() {
-        return photoPath;
-    }
-
-    public void setPhotoPath(String photoPath) {
-        this.photoPath = photoPath;
-    }
-
-    public String getAadhaarFilePath() {
-        return aadhaarFilePath;
-    }
-
-    public void setAadhaarFilePath(String aadhaarFilePath) {
-        this.aadhaarFilePath = aadhaarFilePath;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getAadhaarNumber() {
-        return aadhaarNumber;
-    }
-
-    public void setAadhaarNumber(String aadhaarNumber) {
-        this.aadhaarNumber = aadhaarNumber;
-    }
-
-    public String getBloodGroup() {
-        return bloodGroup;
-    }
-
-    public void setBloodGroup(String bloodGroup) {
-        this.bloodGroup = bloodGroup;
-    }
-
-    public String getAdmissionType() {
-        return admissionType;
-    }
-
-    public void setAdmissionType(String admissionType) {
-        this.admissionType = admissionType;
-    }
-
-    public String getTransportRequired() {
-        return transportRequired;
-    }
-
-    public void setTransportRequired(String transportRequired) {
-        this.transportRequired = transportRequired;
-    }
-
-    public String getHostelRequired() {
-        return hostelRequired;
-    }
-
-    public void setHostelRequired(String hostelRequired) {
-        this.hostelRequired = hostelRequired;
-    }
-
-    public String getFatherName() {
-        return fatherName;
-    }
-
-    public void setFatherName(String fatherName) {
-        this.fatherName = fatherName;
-    }
-
-    public String getMotherName() {
-        return motherName;
-    }
-
-    public void setMotherName(String motherName) {
-        this.motherName = motherName;
-    }
-
-    
 }
     
-
